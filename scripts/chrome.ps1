db92f99d8710b139c1fb645d520f2a1e65857efc
Athrun Sun <okampfer@gmail.com>
2017-12-18 00:11:47 +0800

    Fix chrome installation.

--- scripts/chrome.ps1
+++ scripts/chrome.ps1
@@ -5,4 +5,9 @@ $output = 7z x $dir\chrome.7z "-o$dir"
 rm $dir\chrome.7z
 
 Move-Item -Path "$dir\Chrome-bin\chrome.exe" -Destination "$dir\chrome.exe"
-Get-ChildItem -Path "$dir\Chrome-bin\$version\*.*" -Recurse | Move-Item -Destination "$dir"
+
+# Copy then remove, instead of moving directly. Pay attention to `-Container` switch.
+# https://stackoverflow.com/questions/5432290/how-to-use-powershell-copy-item-and-keep-structure
+Get-ChildItem -Path "$dir\Chrome-bin\$version" | Copy-Item -Destination "$dir" -Recurse -Container
+Get-ChildItem -Path "$dir\Chrome-bin\*" -Recurse -Force | Remove-Item -Recurse -Force
+Remove-Item -Path "$dir\Chrome-bin" -Force
